e316083bc5b4b62aefd1e5f5cda869a0fa75f58e
diogo464 <diogo464@d464.sh>
2025-07-14 17:29:09 +0100

    updated image name

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 PUSH=${PUSH:-"0"}
 TAG=${TAG:-"latest"}
-IMAGE=git.d464.sh/infra/storage-csi:$TAG
+IMAGE=cr.d464.sh/infra/storage-csi:$TAG
 
 CGO_ENABLED=0 go build || exit 1
 docker build -t $IMAGE -f Containerfile .
